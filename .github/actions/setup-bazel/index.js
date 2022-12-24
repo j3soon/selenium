d54f1c3c6195972e6df14d4f21a736bbdf8725d8
Alex Rodionov <p0deje@gmail.com>
2022-12-23 16:27:36 -0800

    Create dir

--- .github/actions/setup-bazel/index.js
+++ .github/actions/setup-bazel/index.js
@@ -87,10 +87,12 @@ async function restoreCache (cacheConfig) {
       )
       const globbedPaths = await globber.glob()
 
+      await io.mkdirP(config.paths.bazelExternal)
       for (const path of globbedPaths) {
         console.log(`Moving ${path} to ${config.paths.bazelExternal}`)
         await io.mv(path, config.paths.bazelExternal, { recursive: true })
       }
+      await io.rmRF(cacheConfig.packageTo)
     }
   } else {
     console.log(`Failed to restore ${name} cache`)
